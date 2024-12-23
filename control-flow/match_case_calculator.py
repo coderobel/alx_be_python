618c6c1f2bd24642acbf27446a8a59fc8443f5f5
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2024-12-23 15:32:33 +0300

    alx projects

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,1 +1,20 @@
-
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+operation = input("Choose the operation (+, -, *, /):")
+sum = 0
+product = 0
+quotient = 0
+difference = 0
+match operation:
+    case "+":
+        sum = int(num1) + int(num2)
+        print("The result is " + str(sum))
+    case "-":
+        difference = int(num1) - int(num2)
+        print("The result is " + str(difference))
+    case "*":
+        product = int(num1) * int(num2)
+        print("The result is " + str(product))
+    case "/":
+        quotient =  int(num1) / int(num2)
+        print("The result is " + str(quotient))
--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,1 +1,5 @@
-
+number = int(input("Enter a number to see its multiplication table: "))
+for i in range(1,11):
+    product = number * i
+    print(str(number) + " * "+ str(i) + " = " + str(product))
+    i = i + 1
