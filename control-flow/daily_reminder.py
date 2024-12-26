18fc62342e14c6d0228bbd4c05e3e3db696e9fe7
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2024-12-26 08:49:22 +0300

    alx projects

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,1 +1,26 @@
+variable = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time bound?: ")
+match priority:
+    case "high":
+        if time_bound:
+            print(variable + " is a " + priority + "  task that requires immediate attention today!" )
+        else:
+            print(variable + " is a " + priority +  " priority task.Consider completing it when you have free time")
+    case "medium":
+        if time_bound:
+            print(variable + " is a " + priority + " priority task that requires immediate attention today")
+        else:
+            print(variable + " is a " + priority +  " priority task.Consider completing it when you have free time.")
+    case "low":
+        if time_bound:
+            print(variable + " is a " + priority +" priority task that.requires immediate attention today")
+                  
+        else:
+            print(variable + " is a " + priority +  " priority task.Consider completing it when you have free time.")
+    case _:
+        print("Invalid data entered")
+
+
+
 
--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,8 @@
+rows = int(input("Enter the size of the pattern: "))
+num = 0
+while int(num) < rows:
+    for i in range(1, rows + 1):
+        print("*",end="")
+    print()
+    num += 1
 
