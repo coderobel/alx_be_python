84f68e16a03da3c936f2ee0d882e6f6adf18a804
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-01-13 21:37:51 +0300

    alx be python project

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,25 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if (num2 == 0):
+                return "indivisible"
+            else:
+                return num1 / num2
+        case _:
+            return "operation not available"
 
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
+main()
