f88a147c951d59ae135130c5ad2b4963f25d519d
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-01-17 21:46:17 +0300

    alx project

--- fns_and_dsa/my_list = [10, 20, 30, 40, 50].py
+++
@@ -1,8 +0,0 @@
-# math_functions.py (Module)
-def add(x, y):
-  """Returns the sum of two numbers."""
-  return x + y
-
-def subtract(x, y):
-  """Returns the difference of two numbers."""
-  return x - y
