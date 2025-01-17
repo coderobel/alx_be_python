a6230e71e34568df7f29070a74c32f66d25e52a3
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-01-17 22:18:57 +0300

    alx project

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,16 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+def convert_to_celsius(fahrenheit):
+    return((fahrenheit -32) *  FAHRENHEIT_TO_CELSIUS_FACTOR)
+def convert_to_fahrenheit(celsius):
+    return((celsius *  CELSIUS_TO_FAHRENHEIT_FACTOR ) + 32)
+def main():
+    number = input("Enter the temperature to convert: ")
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+    if unit == "C":
+        result = convert_to_fahrenheit(float(number))
+        print(str(number) + unit + "is " + str(result)+ "F")
+    elif unit == "F":
+        result = convert_to_celsius(float(number))
+        print(str(number) + unit + "is " + str(result)+ "C")
+main()
